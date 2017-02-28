5bfea6e3c5ee763ad23dd059895cf537a5f82338
levro <levro@ukr.net>
2017-02-28 15:28:37 +0200

    random matrix 6x6

--- src/matr/matr.java
+++ src/matr/matr.java
@@ -3,8 +3,18 @@ package matr;
 public class matr {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int [][] matr;
+		matr = new int [6][6];
+		for (int i=0; i<6; i++){
+			for (int j=0; j<6; j++){
+				matr[i][j] = (int) (Math.random()*99);
+				if (matr[i][j]<10)
+					System.out.print(" " + matr[i][j] + " ");
+				else
+					System.out.print(matr[i][j] + " ");
+			}
+			System.out.println("");
+		}		
 	}
 
 }
