fcc5f1cfc21e552a0763a81bcbceac247b00c1d0
levro <levro@ukr.net>
2017-02-28 15:55:41 +0200

    MART MART

--- src/matr/matr.java
+++ src/matr/matr.java
@@ -3,10 +3,12 @@ package matr;
 public class matr {
 
 	public static void main(String[] args) {
+		int n = 6;
+		System.out.println("----MATR----");
 		int [][] matr;
-		matr = new int [6][6];
-		for (int i=0; i<6; i++){
-			for (int j=0; j<6; j++){
+		matr = new int [n][n];
+		for (int i=0; i<n; i++){
+			for (int j=0; j<n; j++){
 				matr[i][j] = (int) (Math.random()*99);
 				if (matr[i][j]<10)
 					System.out.print(" " + matr[i][j] + " ");
@@ -14,7 +16,22 @@ public class matr {
 					System.out.print(matr[i][j] + " ");
 			}
 			System.out.println("");
-		}		
+		}
+		
+		System.out.println("");
+		System.out.println("----MART----");
+		
+		int [][] mart;
+		mart = new int [n][n];
+		for (int i=0; i<n; i++){
+			for (int j=0; j<n; j++){
+				mart[i][j] = (int) (Math.random()*99);
+				if (mart[i][j]<10)
+					System.out.print(" " + mart[i][j] + " ");
+				else
+					System.out.print(mart[i][j] + " ");
+			}
+		System.out.println("");
+		}
 	}
-
 }
